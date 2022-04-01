143093524c8eb8a32a74de5cdb98ac37dadca962
João Paulo Moraes <joaopaulomoraes@outlook.com>
2022-04-01 12:06:21 -0300

    Improve tests description

--- test/hi_mom_test.exs
+++ test/hi_mom_test.exs
@@ -2,19 +2,23 @@ defmodule HiMomTest do
   use ExUnit.Case
   doctest HiMom
 
-  test "hi mom" do
+  test "should say hi mom" do
     assert HiMom.hi_mom() == "Hi, mom!"
   end
 
-  test "hi kate" do
+  test "should say hi kate" do
     assert HiMom.hi_mom("Kate") == "Hi, Kate!"
   end
 
-  test "hi maria from pt" do
+  test "should say hi to maria from pt" do
     assert HiMom.hi_mom("Maria", :pt) == "Oi, Maria!"
   end
 
-  test "hi whithout specify a name from pt" do
+  test "should say hi mom whithout specify a name from pt" do
     assert HiMom.hi_mom(nil, :pt) == "Oi, mãe!"
+  end
+
+  test "should not say hi dad" do
+    assert HiMom.hi_mom("dad") != "Hi, mom!"
   end
 end
